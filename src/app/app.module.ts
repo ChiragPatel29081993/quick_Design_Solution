8393500c79995c720a644ad8f0b75c771e9e18f6
ChiragPatel29081993 <quickdesignsolution@gmail.com>
2024-02-17 23:38:51 +0530

    secound commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { AdminProfileComponent } from './components/admin-profile/admin-profile.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PaymentInfoComponent } from './components/payment-info/payment-info.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ShareComponent } from './components/share/share.component';
 @NgModule({
   declarations: [
     AppComponent,
     AdminProfileComponent,
     AboutUsComponent,
-    FooterComponent
+    FooterComponent,
+    PaymentInfoComponent,
+    GalleryComponent,
+    ShareComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/admin-profile/admin-profile.component.ts
+++ src/app/components/admin-profile/admin-profile.component.ts
@@ -29,7 +29,7 @@ export class AdminProfileComponent implements OnInit {
     const uri = `tel:${phoneNumber}`;
     window.open(uri, '_self');
   }
-  
+
   openMap() {
     // Define the address
     const address = 'C-13, Shivsagar Society, Vastral Road, Vastral, Ahmedabad-382418'; // Add your address here
@@ -38,6 +38,22 @@ export class AdminProfileComponent implements OnInit {
     const uri = `https://www.google.com/maps/place/shivsagar+soc,+C%2F13,+Vastral+Road,+Kailashdham+Society,+Mahadev+Nagar+Tekra,+Ahmedabad,+Gujarat+382418/@23.0012296,72.6521097,17.55z/data=!4m14!1m7!3m6!1s0x395e8637e2565197:0xcd80afb78ffde3a3!2sShivsagar+Society!8m2!3d23.00226!4d72.6515287!16s%2Fg%2F1pp2wxzkx!3m5!1s0x395e87782bdddc0d:0x168bbbeee485ea56!8m2!3d23.0013644!4d72.6515083!16s%2Fg%2F11sd7nd180?entry=ttu`;
 
     // Open the map application
+    window.open(uri, '_blank');
+  }
+
+  sendEmail() {
+    // Define the recipient email address
+    const recipientEmail = 'quickdesignsolution@gmail.com'; // Add your recipient email here
+
+    // Construct the mailto URI
+    const uri = `mailto:${recipientEmail}`;
+
+    // Open the default email client
+    window.open(uri, '_blank');
+  }
+
+  openWeb() {
+    const uri = `https://quickdesignsolution.netlify.app/`;
     window.open(uri, '_blank');
   }
 }
