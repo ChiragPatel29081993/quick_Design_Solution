961dda8b58deaf26e5d98ec9934b67a64856b89d
ChiragPatel29081993 <quickdesignsolution@gmail.com>
2024-02-18 01:21:11 +0530

    galary and payment part

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
+  isModalOpen: boolean = false;
+  modalImageUrl: string = '';
+  modalImageCaption: string = '';
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  openImageModal(imageUrl: string, caption: string) {
+    this.modalImageUrl = ''
+    this.modalImageUrl = imageUrl;
+    this.modalImageCaption = caption;
+    this.isModalOpen = true;
+  }
+
+  closeImageModal() {
+    this.isModalOpen = false;
+  }
 }
